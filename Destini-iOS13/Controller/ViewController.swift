01a44d38aa90d53ea6f540a086708448d40d455c
gabrielCastilloSerafim <castillo.serafim.gabriel@gmail.com>
2022-08-23 11:44:23 +0200

    complete app using MVC desing pattern

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        //Check answer
+        let userAnswer = sender.currentTitle
+        
+        storyBrain.checkAnswer(userAnswer: userAnswer!)
+        
+        updateUI()
+        
+    }
+   
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        firstButton.setTitle(storyBrain.getButtonChoice1(), for: .normal)
+        secondButton.setTitle(storyBrain.getButtonChoice2(), for: .normal)
+    }
 }
 
